150c459906621daabbd2b0d4136fd8c5f6b58072
Jordan Mack <jordan.mack@brilliantnotion.com>
2021-04-05 00:23:09 -0700

    Simplify aggdoublecounter.

--- contracts/aggdoublecounter/src/entry.rs
+++ contracts/aggdoublecounter/src/entry.rs
@@ -4,7 +4,6 @@ use core::result::Result;
 // Import heap related library from `alloc`
 // https://doc.rust-lang.org/alloc/index.html
 use alloc::vec::Vec;
-use alloc::collections::btree_set::BTreeSet;
 
 // Import CKB syscalls and structures
 // https://nervosnetwork.github.io/ckb-std/riscv64imac-unknown-none-elf/doc/ckb_std/index.html
@@ -35,54 +34,33 @@ pub fn main() -> Result<(), Error>
 
 	// Retrieve all the group input and group output cell data.
 	let group_input_data: Vec<_> = QueryIter::new(load_cell_data, Source::GroupInput).collect();
-	let group_output_data: Vec<_> = QueryIter::new(load_cell_data, Source::GroupOutput).collect();
-
-	// Track the group output cells that already been matched.
-	let mut group_outputs_matched: BTreeSet<usize> = BTreeSet::new();
 
 	// Loop through all the group input cell data.
-	'input_loop: for input_data in group_input_data
+	for (i, input_data) in group_input_data.iter().enumerate()
 	{
-		// Load the first input cell data value and convert the data into a u64 value.
+		// Load the output data at the same index.
+		let output_data = load_cell_data(i, Source::GroupOutput)?;
+
+		// Convert the input cell data into u64 values.
 		let mut buffer = [0u8; 8];
 		buffer.copy_from_slice(&input_data[0..8]);
 		let input_value_1 = u64::from_le_bytes(buffer);
-
-		// Load the second input cell data value and convert the data into a u64 value.
-		let mut buffer = [0u8; 8];
 		buffer.copy_from_slice(&input_data[8..16]);
 		let input_value_2 = u64::from_le_bytes(buffer);
 
-		// Loop through all the group output cell data.
-		for (o, output_data) in group_output_data.iter().enumerate()
+		// Convert the output cell data into a u64 values.
+		let mut buffer = [0u8; 8];
+		buffer.copy_from_slice(&output_data[0..8]);
+		let output_value_1 = u64::from_le_bytes(buffer);
+		buffer.copy_from_slice(&output_data[8..16]);
+		let output_value_2 = u64::from_le_bytes(buffer);
+
+		// Check if the output is one more than the input.
+		if input_value_1 + 1 != output_value_1 || input_value_2 + 2 != output_value_2
 		{
-			// If the group output cell has not already matched previously.
-			if !group_outputs_matched.contains(&o)
-			{
-				// Load the first output cell data and convert the data into a u64 value.
-				let mut buffer = [0u8; 8];
-				buffer.copy_from_slice(&output_data[0..8]);
-				let output_value_1 = u64::from_le_bytes(buffer);
-
-				// Load the second output cell data and convert the data into a u64 value.
-				let mut buffer = [0u8; 8];
-				buffer.copy_from_slice(&output_data[8..16]);
-				let output_value_2 = u64::from_le_bytes(buffer);
-
-				// Check if the output is one more than the input.
-				if input_value_1 + 1 == output_value_1 && input_value_2 + 2 == output_value_2
-				{
-					// Mark this output as matched.
-					group_outputs_matched.insert(o);
-
-					// Continue to the next group input cell data.
-					continue 'input_loop;
-				}
-			}
+			// If no match was found return an error.
+			return Err(Error::InvalidCounterValue);
 		}
-
-		// If no match was found return an error.
-		return Err(Error::InvalidCounterValue);
 	}
 
 	// Return success if all group input and output cells have been checked and no errors were found.
